2e3b6e1a4a7b0d6ceb9c318ca7fe9af5908e1cba
Ale504 <ale_alpha_504@hotmail.com>
2021-03-24 13:24:39 -0300

    movere

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
+  return(
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/Ale504.png" alt="Alexandre Bueno"/>
+      <div>
+        <strong>Alexandre Bueno</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level"/>
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
